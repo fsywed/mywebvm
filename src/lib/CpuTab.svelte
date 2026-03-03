0da3c8148ab889bb91c0f41ccec57f1e438a5f7c
fsywed <fsywed@noreply.gitcode.com>
2026-03-03 21:25:56 +0800

    hi
    Signed-off-by: fsywed <fsywed@noreply.gitcode.com>

--- src/lib/CpuTab.svelte
+++ src/lib/CpuTab.svelte
@@ -3,10 +3,10 @@
 	import { cpuPercentage } from './activities.js'
 </script>
 
-<h1 class="text-lg font-bold">Engine</h1>
-<PanelButton buttonImage="assets/cheerpx.svg" clickUrl="https://cheerpx.io/docs" buttonText="Explore CheerpX">
+<h1 class="text-lg font-bold">引擎</h1>
+<PanelButton buttonImage="assets/cheerpx.svg" clickUrl="https://cheerpx.io/docs" buttonText="探索 CheerpX">
 </PanelButton>
-<p><span class="font-bold">Virtual CPU: </span>{$cpuPercentage}%</p>
-<p>CheerpX is a x86 virtualization engine in WebAssembly</p>
-<p>It can securely run unmodified x86 binaries and libraries in the browser</p>
-<p>Excited about our technology? <a class="underline" href="https://cheerpx.io/docs/getting-started" target="_blank">Start building</a> your projects using <a class="underline" href="https://cheerpx.io/" target="_blank">CheerpX</a> today!</p>
+<p><span class="font-bold">虚拟 CPU：</span>{$cpuPercentage}%</p>
+<p>CheerpX 是一个基于 WebAssembly 的 x86 虚拟化引擎</p>
+<p>它可以在浏览器中安全运行未经修改的 x86 二进制文件和库</p>
+<p>对我们的技术感兴趣？今天就使用 <a class="underline" href="https://cheerpx.io/" target="_blank">CheerpX</a> <a class="underline" href="https://cheerpx.io/docs/getting-started" target="_blank">开始构建</a> 你的项目吧！</p>
