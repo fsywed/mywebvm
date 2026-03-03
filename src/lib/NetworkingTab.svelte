d0b552b407b776fd8544a0d96cdf5989e7d9c56f
fsywed <fsywed@noreply.gitcode.com>
2026-03-03 21:26:06 +0800

    hi
    Signed-off-by: fsywed <fsywed@noreply.gitcode.com>

--- src/lib/NetworkingTab.svelte
+++ src/lib/NetworkingTab.svelte
@@ -14,11 +14,11 @@
 	let buttonData = null;
 	$: buttonData = updateButtonData($connectionState, handleConnect);
 </script>
-<h1 class="text-lg font-bold">Networking</h1>
+<h1 class="text-lg font-bold">网络连接</h1>
 <PanelButton buttonImage="assets/tailscale.svg" clickUrl={buttonData.clickUrl} clickHandler={buttonData.clickHandler} rightClickHandler={buttonData.rightClickHandler} buttonTooltip={buttonData.buttonTooltip} buttonText={buttonData.buttonText}>
 	{#if $connectionState == "CONNECTED"}
-		<i class='fas fa-circle fa-xs ml-auto {$exitNode ? 'text-green-500' : 'text-amber-500'}' title={$exitNode ? 'Ready' : 'No exit node'}></i>
+		<i class='fas fa-circle fa-xs ml-auto {$exitNode ? 'text-green-500' : 'text-amber-500'}' title={$exitNode ? '就绪' : '未配置出口节点'}></i>
 	{/if}
 </PanelButton>
-<p>WebVM can connect to the Internet via Tailscale</p>
-<p>Using Tailscale is required since browser do not support TCP/UDP sockets (yet!)</p>
+<p>WebVM 可以通过 Tailscale 连接到互联网</p>
+<p>由于浏览器目前尚不支持 TCP/UDP 套接字，因此需要使用 Tailscale 来实现网络连接。</p>
