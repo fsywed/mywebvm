3ab40dc9809489b661c7384df07905be1102aa10
fsywed <fsywed@noreply.gitcode.com>
2026-03-05 21:44:30 +0800

    update: 更新文件 DiscordTab.svelte
    Signed-off-by: fsywed <fsywed@noreply.gitcode.com>

--- src/lib/DiscordTab.svelte
+++ src/lib/DiscordTab.svelte
@@ -4,9 +4,9 @@
 </script>
 
 <h1 class="text-lg font-bold">Discord</h1>
-<PanelButton buttonImage="assets/discord-mark-blue.svg" clickUrl="https://discord.gg/yTNZgySKGa" buttonText="Join our Discord">
+<PanelButton buttonImage="assets/discord-mark-blue.svg" clickUrl="https://discord.gg/yTNZgySKGa" buttonText="加入我们的 Discord">
 	<i class='fas fa-circle fa-xs ml-auto text-green-500'></i>
 	<span class="ml-1"><DiscordPresenceCount /></span>
 </PanelButton>
-<p>Do you have any question about WebVM or CheerpX?</p>
-<p>Join our community, we are happy to help!</p>
+<p>你有关于 WebVM 或 CheerpX 的问题吗？</p>
+<p>加入我们的社区，我们很乐意为你提供帮助！</p>
